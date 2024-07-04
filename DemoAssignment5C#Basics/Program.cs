845832bda04d047847f7b8f60eee4c219154f178
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-04 22:54:59 +0300

    example 3 added

--- DemoAssignment5C#Basics/Program.cs
+++ DemoAssignment5C#Basics/Program.cs
@@ -23,6 +23,30 @@
             Console.WriteLine(Y);
 
             #endregion
+
+            #region example3
+            //int x = null; // Valid
+            // int x => x Allow only int numbers
+
+            //int? Y = null; // Valid
+            // int? Y => Y Allow Int Values + Null
+
+            int? YY = null;
+            int X = 10;
+
+            //int? Z = Y; // Valid
+            int? Z = X; // Valid
+
+            int? Number; //Nullable<int> Number;    // Nullable -> Allow Null + Values
+
+            //decimal Num01 = 1.1M;
+            //decimal? Num02 = Num01;
+
+            decimal? Num01 = null;
+
+            Console.WriteLine(Num01);
+            Console.WriteLine(YY); 
+            #endregion
         }
     }
 }
