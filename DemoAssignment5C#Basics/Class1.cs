520a6f226882c3ebae85ec0864882e89e7d6e7e5
Marwa Mahmoud <mm3764453@gmail.com>
2024-07-04 22:45:01 +0300

    example:2

---
+++ DemoAssignment5C#Basics/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAssignment5C_Basics
+{
+    internal class Class1
+    {
+    }
+}
---
+++ DemoAssignment5C#Basics/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DemoAssignment5C_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region example1: casting
+            object o1;
+            o1 = "Ahmed"; // Casting
+
+            o1=1; // Casting [Value Type > Reference Type] Boxing
+            o1 = 'A'; // Casting [Value Type -> Reference Type] Boxing
+            o1 = new Class1(); // Casting
+            #endregion
+
+            #region example2
+
+            //int X = 10;          //obj new object(10);// Boxing [Safe Casting]
+            //object obj = X;
+
+            object obj02 = 10;                                        
+            int Y = (int)obj02;   // Casting Unboxing [Unsafe Casting]
+            Console.WriteLine(Y);
+
+            #endregion
+        }
+    }
+}
